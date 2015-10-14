e42837b75eaeb2304dc047382250eb1aa410739b
lasttry <rsantos@parallels.com>
2015-10-14 11:53:35 +0200

    first commit

---
+++ APS.CSharp.Runtime/Database/Bindings.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+
+namespace APS.CSharp.Runtime.Database
+{
+    public class Bindings
+    {
+        [PrimaryKey]
+        public string InstanceId { get; set; }
+        public string APSType { get; set; }
+        public string ControllerUri { get; set; }
+        public string CertificateSelf { get; set; }
+        public string CertificateController { get; set; }
+
+        public Bindings() { }
+        public Bindings(Bindings binding)
+        {
+            InstanceId = binding.InstanceId;
+            APSType = binding.APSType;
+            ControllerUri = binding.ControllerUri;
+            CertificateSelf = binding.CertificateSelf;
+            CertificateController = binding.CertificateController;
+        }
+    }
+}
